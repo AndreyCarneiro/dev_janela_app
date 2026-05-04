0903b48cf4968e7b384dcd74e20d2f72fa6fc1a9
Dev <andreycarneiro786@gmail.com>
2026-05-04 14:55:26 -0300

    2

---
+++ projeto_and_1per/aula1.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import ttk
+
+def main():
+    #Criação da janela "Pai"
+    root = tk.Tk()
+    root.tittle("Minha Primeira Janela")
+    root.geometry("400x200")
+    root
