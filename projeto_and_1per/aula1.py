199a1f194fcfbacf90f84ed2d9ff42d8368386c7
Dev <andreycarneiro786@gmail.com>
2026-05-04 16:36:36 -0300

    3

--- projeto_and_1per/aula1.py
+++ projeto_and_1per/aula1.py
@@ -2,8 +2,22 @@ import tkinter as tk
 from tkinter import ttk
 
 def main():
-    #Criação da janela "Pai"
+    #Criação da janela "Pai/Raiz"
     root = tk.Tk()
-    root.tittle("Minha Primeira Janela")
+    root.title("Minha Primeira Janela")
     root.geometry("400x200")
-    root
+    root.resizable(True, True)
+
+    # label simple (Etiqueta)
+    label = ttk.Label(root, text="Aerials", font=("Georgia", 16,"bold"))
+    label.pack(expand=True)
+
+    # Botão para fechar a janela
+    btn = ttk.Button(root, text="Fechar", command=root.destroy)
+    btn.pack(anchor="s", padx=20)
+    
+    # Incia o loop de eventos
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
